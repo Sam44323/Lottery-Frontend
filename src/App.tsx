59eaad82c1f0683cf9c742c95da0aa9d3c428b1e
Suranjan <suranjanbiswas2002@gmail.com>
2021-06-20 20:49:46 +0530

    add: added node-sass to project and changes css to scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 
-function App() {
-  return <div className="App">Learn React</div>;
-}
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Learn-React</h1>
+    </div>
+  );
+};
 
 export default App;
